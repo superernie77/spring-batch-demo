82025c4753212c7015ec2f15dbcb3d149e0e3462
Ernesto Elias-Nieland <superernie77@gmail.com>
2023-10-04 08:18:09 +0200

    testdata file added

--- src/test/java/com/example/demo/BatchEnd2EndTest.java
+++ src/test/java/com/example/demo/BatchEnd2EndTest.java
@@ -8,7 +8,6 @@ import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 @SpringBatchTest
 @SpringBootTest
